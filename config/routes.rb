44f58f0f094dddb6414570524b1aec06df9f17cb
Canan <cananercan96@gmail.com>
2020-05-23 18:01:10 +0300

    Add devise controllers for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+  }
+
   resources :users, only: [:show, :index]
   root 'welcome#index'
   get 'teams', to: 'teams#index'
-  post 'login', to: 'sessions#create'
+  # post 'login', to: 'sessions#create'
 end
