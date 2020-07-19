6ef3bc754819018b3a6702e834775736cebcc199
Canan <canan.ercan@parasut.com>
2020-07-19 18:19:33 +0300

    Change answer and entry models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,26 +4,14 @@ class Answer < ApplicationRecord
   has_many :entries, dependent: :destroy
 
   def positive_entries
-    positive_entries = []
-    self.entries.each do |entry|
-      positive_entries.push(entry) if entry.positive?
-    end
-    positive_entries
+    self.entries.select { |entry| entry.positive? }
   end 
 
   def neutral_entries
-    neutral_entries = []
-    self.entries.each do |entry|
-      neutral_entries.push(entry) if entry.neutral?
-    end
-    neutral_entries
+    self.entries.select { |entry| entry.neutral? }
   end
 
   def negative_entries
-    negative_entries = []
-    self.entries.each do |entry|
-      negative_entries.push(entry) if entry.negative?
-    end
-    negative_entries
+    self.entries.select { |entry| entry.negative? }
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,11 +1,12 @@
 class Entry < ApplicationRecord
+  # enum type: [:positive, :neutral, :negative]
   belongs_to :answer
   validates :detail, presence: true, length: { minimum: 2, maximum: 255 }
 
   def positive?
     entry_type == 'positive'
   end
-  
+
   def neutral?
     entry_type == 'neutral'
   end
